db5ebb2a9d2061ca4b952d8111f9c1707879af79
Christian Coenen <christian300507@yahoo.com>
2026-05-05 12:06:38 +0200

    layout(footer): add footer
    - Add footer including styling.
    - Standardize brand font CSS variable.
    - Adjust hero title's font.
    - Adjust opacity values when hovering over cards.
    - Add hover animation to latest collection card.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,14 @@
+import './Footer.css'
+
 const Footer = () => {
-    <footer>
-        
-    </footer>
+    return (
+        <footer className="footer">
+            <div className="footer-inner">
+                <p className="footer-text">
+                    Copyright © 2026 ARTIXIC. Created by <a className="footer-link" href="https://christian-coenen-portfolio.vercel.app/" target="_blank">Christian Coenen</a>.</p>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
