6f69f93e9ceddd9fdde7b28276a7a1d2e73d0e5f
Christian Coenen <christian300507@yahoo.com>
2026-09-15 12:01:22 +0200

    feat(collection-detail): add artwork grid section
    - Add artwork grid section to collection detail page

--- src/pages/visitor-pages/CollectionDetailPage.jsx
+++ src/pages/visitor-pages/CollectionDetailPage.jsx
@@ -1,10 +1,12 @@
 import VisitorLayout from '../../components/layout/VisitorLayout'
 import CollectionDetailCoverSection from '../../components/page-sections/collection-detail/CollectionDetailCoverSection'
+import CollectionDetailGridSection from '../../components/page-sections/collection-detail/CollectionDetailGridSection'
 
 const CollectionDetailPage = () => {
     return (
         <VisitorLayout>
             <CollectionDetailCoverSection />
+            <CollectionDetailGridSection />
         </VisitorLayout>
     )
 }
