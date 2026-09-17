8e57f9f0a6076c5e759a18b9333f7b7d507ddc02
Christian Coenen <christian300507@yahoo.com>
2026-09-17 09:49:21 +0200

    feat(about-page): add about intro section
    - Add about intro section

---
+++ src/components/page-sections/about/AboutIntroSection.jsx
@@ -0,0 +1,25 @@
+import './AboutIntroSection.css'
+
+const AboutIntroSection = () => {
+    return (
+        <section className="about-intro-section">
+            <div className="about-intro-section-background" />
+
+            <div className="about-intro-inner">
+                <p className="about-intro-eyebrow">
+                    About
+                </p>
+
+                <h3 className="about-intro-header">
+                    A digital space for contemporary art
+                </h3>
+
+                <p className="about-intro-body">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+            </div>            
+        </section>
+    )
+}
+
+export default AboutIntroSection
--- src/pages/visitor-pages/AboutPage.jsx
+++ src/pages/visitor-pages/AboutPage.jsx
@@ -1,7 +1,12 @@
-const AboutPage = () => {
-    return {
+import VisitorLayout from '../../components/layout/VisitorLayout.jsx'
+import AboutIntroSection from '../../components/page-sections/about/AboutIntroSection'
 
-    }
+const AboutPage = () => {
+    return (
+        <VisitorLayout>
+            <AboutIntroSection />
+        </VisitorLayout>
+    )
 }
 
 export default AboutPage
