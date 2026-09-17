6d559e09973dace5ecd9282c60cbb74d550aa4a8
Christian Coenen <christian300507@yahoo.com>
2026-09-17 13:37:25 +0200

    feat(about-page): add exhibition section
    - Add exhibition section

--- src/pages/visitor-pages/AboutPage.jsx
+++ src/pages/visitor-pages/AboutPage.jsx
@@ -1,12 +1,14 @@
 import VisitorLayout from '../../components/layout/VisitorLayout.jsx'
 import AboutIntroSection from '../../components/page-sections/about/AboutIntroSection'
 import AboutMissionSection from '../../components/page-sections/about/AboutMissionSection'
+import AboutExhibitionSection from '../../components/page-sections/about/AboutExhibitionSection.jsx'
 
 const AboutPage = () => {
     return (
         <VisitorLayout>
             <AboutIntroSection />
             <AboutMissionSection />
+            <AboutExhibitionSection />
         </VisitorLayout>
     )
 }
