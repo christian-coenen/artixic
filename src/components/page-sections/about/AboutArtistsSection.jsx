affe0559086b7464dbe8f4e54791fcbd7a7d9839
Christian Coenen <christian300507@yahoo.com>
2026-09-21 09:40:28 +0200

    feat(about-page): add artists section
    - Add artists section

---
+++ src/components/page-sections/about/AboutArtistsSection.jsx
@@ -0,0 +1,23 @@
+import './AboutArtistsSection.css'
+
+const AboutArtistsSection = () => {
+    return (
+        <section className="about-artists-section">
+            <div className="about-artists-inner">
+                <p className="about-artists-eyebrow">
+                    Artists
+                </p>
+
+                <h2 className="about-artists-heading">
+                    A place for artists to be seen
+                </h2>
+
+                <p className="about-artists-body">
+                    Artists can share their work regardless of their background, experience, or established recognition. Whether presenting a single artwork or an entire collection, ARTIXIC provides the space to develop a presence and let the work speak for itself.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default AboutArtistsSection
--- src/pages/visitor-pages/AboutPage.jsx
+++ src/pages/visitor-pages/AboutPage.jsx
@@ -2,6 +2,7 @@ import VisitorLayout from '../../components/layout/VisitorLayout.jsx'
 import AboutIntroSection from '../../components/page-sections/about/AboutIntroSection'
 import AboutMissionSection from '../../components/page-sections/about/AboutMissionSection'
 import AboutExhibitionSection from '../../components/page-sections/about/AboutExhibitionSection.jsx'
+import AboutArtistsSection from '../../components/page-sections/about/AboutArtistsSection.jsx'
 
 const AboutPage = () => {
     return (
@@ -9,6 +10,7 @@ const AboutPage = () => {
             <AboutIntroSection />
             <AboutMissionSection />
             <AboutExhibitionSection />
+            <AboutArtistsSection />
         </VisitorLayout>
     )
 }
